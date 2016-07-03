2b22a78d6fdda4970e9fe468cd3dcc79b4cafe31
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-07-03 23:47:56 +0900

    refactor jQuery

--- src/pc/js/vendor.js
+++ src/pc/js/vendor.js
@@ -1,3 +1,5 @@
-require('../lib/jquery-2.2.0.min.js');
+import $ from '../lib/jquery-2.2.0.min.js';
+window.$ = window.jQuery = $;
+
 require('../lib/sns/facebook.js');
 require('../lib/sns/twitter.js');
