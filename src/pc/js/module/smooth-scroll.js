41ee124b012323cc217a854f52cdef618ec74d27
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-07-03 18:04:25 +0900

    create src/

---
+++ src/pc/js/module/smooth-scroll.js
@@ -0,0 +1,33 @@
+export default function smoothScroll() {
+
+    'use strict';
+
+    const HEADER_HEIGHT = 0;
+
+    let $document = $(document);
+
+    let mStopOn = () => {
+        $document.on('DOMMouseScroll', preventDefault);
+        $document.on('mousewheel', preventDefault);
+    };
+
+    let mStopOff = () => {
+        $document.off('DOMMouseScroll', preventDefault);
+        $document.off('mousewheel', preventDefault);
+    };
+
+    let preventDefault = (event) => {
+        event.preventDefault();
+    };
+
+    $('a.js-scroll').click(function (event) {
+        if (!$(this).hasClass('nolink')) {
+            let id = $(this).attr('href'),
+                target = $(id).offset().top;
+            mStopOn();
+            $('html, body').animate({scrollTop: target - HEADER_HEIGHT}, 500, mStopOff);
+            event.preventDefault();
+            return false;
+        }
+    });
+}
