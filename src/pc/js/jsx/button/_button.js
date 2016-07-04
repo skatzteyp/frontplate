94d17cc718da4cb376c1c9da3ea05951f8650654
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-07-04 09:57:10 +0900

    button sample

--- src/pc/js/jsx/button/_button.js
+++ src/pc/js/jsx/button/_button.js
@@ -1,12 +1,23 @@
 class SampleComponent extends React.Component {
+    constructor(pops){
+        super(pops);
+
+        this.setClass();
+    }
+
+    setClass() {
+        this.className += ' is-active';
+    }
+
     render() {
         const {text} = this.props;
+
         return (
-            React.createElement("button", null, text)
+            React.createElement("button", {className: "button"}, text)
         );
     }
 }
 
 const $sample = document.getElementById('js-sample');
 
-ReactDOM.render(React.createElement(SampleComponent, {text: "button"}), $sample);
+ReactDOM.render(React.createElement(SampleComponent, {text: "button", onClick: "{this.setClass}"}), $sample);
--- src/pc/js/jsx/button/_button.jsx
+++ src/pc/js/jsx/button/_button.jsx
@@ -1,12 +1,23 @@
 class SampleComponent extends React.Component {
+    constructor(pops){
+        super(pops);
+
+        this.setClass();
+    }
+
+    setClass() {
+        this.className += ' is-active';
+    }
+
     render() {
         const {text} = this.props;
+
         return (
-            <button>{text}</button>
+            <button className="button">{text}</button>
         );
     }
 }
 
 const $sample = document.getElementById('js-sample');
 
-ReactDOM.render(<SampleComponent text="button" />, $sample);
+ReactDOM.render(<SampleComponent text="button" onClick="{this.setClass}" />, $sample);
