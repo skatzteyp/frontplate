2231fc0daa2d53b41ea077b9d2f5223736320ab0
堀口 誠人 <horiguchi_seito@liginc.co.jp>
2016-10-14 18:33:39 +0900

    update jQuery

--- src/pc/js/vendor.js
+++ src/pc/js/vendor.js
@@ -1,5 +1,5 @@
 require('../lib/sns/facebook.js');
 require('../lib/sns/twitter.js');
 
-import $ from '../lib/jquery-2.2.0.min.js';
+import $ from '../lib/jquery-3.1.1.min.js';
 window.$ = window.jQuery = $;
