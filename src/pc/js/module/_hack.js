15ccbfdc2a125cf92fff205ba1bd58564ca24ef8
堀口 誠人 <horiguchi_seito@liginc.co.jp>
2016-10-18 16:59:59 +0900

    add hack

--- src/pc/js/module/_hack.js
+++ src/pc/js/module/_hack.js
@@ -6,21 +6,65 @@ export default function hack() {
     let htmlTag = document.documentElement;
 
     //userAgentを変数に
-    let userAgent = window.navigator.userAgent.toLowerCase();
+    let userAgent = window.navigator.userAgent.toLowerCase(),
+        platform = window.navigator.platform.toLowerCase();
 
     //OS,ブラウザを変数に
-    let osList = ['windows'];
+    let uaIe = [/(msie|MSIE)/, /(T|t)rident/];
+
+    //クラス名を変数に
+    let browserClass = ['ie', 'windows', 'safari', 'chrome', 'firefox', 'ie 10', 'edge'];
 
     //スペースを定数に
     let SPACE = ' ';
 
-    //OSを判定してhtmlタグにクラスをつける
-    for (let i = 0, len = osList.length; i < len; i += 1) {
-
-        if (userAgent.indexOf(osList[i]) !== -1) {
-            htmlTag.className += SPACE + osList[i];
-
-            break;
+    //function
+    let addClassHack = (conditions, className) => {
+        if (conditions) {
+            htmlTag.className += SPACE + className;
         }
-    }
+    };
+
+    //ie
+    addClassHack(
+        userAgent.match(uaIe[0]) || userAgent.match(uaIe[1]),
+        browserClass[0]
+    );
+
+    //windows
+    addClassHack(
+        platform.indexOf('win') !== -1,
+        browserClass[1]
+    );
+
+    //safari
+    addClassHack(
+        userAgent.indexOf(browserClass[2]) !== -1 && userAgent.indexOf(browserClass[3]) === -1,
+        browserClass[2]
+    );
+
+    //chrome
+    addClassHack(
+        userAgent.indexOf(browserClass[2]) !== -1 && userAgent.indexOf(browserClass[3]) !== -1,
+        browserClass[3]
+    );
+
+    //firefox
+    addClassHack(
+        userAgent.indexOf(browserClass[4]) !== -1,
+        browserClass[4]
+    );
+
+    //ie10
+    addClassHack(
+        userAgent.indexOf(browserClass[5]) !== -1,
+        'ie10'
+    );
+
+    //edge
+    addClassHack(
+        userAgent.indexOf(browserClass[6]) !== -1,
+        browserClass[6]
+    );
+
 }
